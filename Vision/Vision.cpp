ab894f5700e855eb0fdd9a473f32c9ed1f137bd2
MIAOxinquan <maio913247@163.com>
2020-03-20 16:02:27 +0800

    version 0.2 new plotTab&codeTab; building slots func and func Quit,About&closeEvent basically finished;

--- Vision/Vision.cpp
+++ Vision/Vision.cpp
@@ -1,34 +1,52 @@
 #include "stdafx.h"
 #include "Vision.h"
 
-Vision::Vision(QWidget *parent)
+Vision::Vision(QWidget* parent)
 	: QMainWindow(parent)
 	, timer(new QTimer(this))
 	, curDateTimeLabel(new QLabel())
-	,VSplitter(new QSplitter(Qt::Horizontal , this))
-	,a(new QTextEdit("a", VSplitter))
-	,b(new QTextEdit("b", VSplitter))
-	,c(new QTextEdit("c", VSplitter))
+	, globalSplitter(new QSplitter(Qt::Horizontal, this))
+	, a(new QTextEdit("a", globalSplitter))
+	, plotTab(new QTabWidget(globalSplitter))
+	, codeTab(new QTabWidget(globalSplitter))
 {
 	visionUi.setupUi(this);//ui定义上区：菜单栏和工具栏
 	curDateTimeLabel->setAlignment(Qt::AlignRight);
 	visionUi.statusBar->addPermanentWidget(curDateTimeLabel);
 
 	a->setMinimumWidth(200);
-	b->setMinimumWidth(200);
-	c->setMinimumWidth(200);
+	plotTab->setMinimumWidth(200);
+	codeTab->setMinimumWidth(200);
 	//(index, stretch) 分割器内第index号框内元素stretch 0则不随窗体变化，1+则为比例系数
 	//例如以下1号元素与2号元素宽度比为2：1
-	VSplitter->setStretchFactor(0, 0);
-	VSplitter->setStretchFactor(1, 2);
-	VSplitter->setStretchFactor(2, 1);
+	globalSplitter->setStretchFactor(0, 0);
+	globalSplitter->setStretchFactor(1, 2);
+	globalSplitter->setStretchFactor(2, 1);
 
 
-	this->setCentralWidget(VSplitter);
+	this->setCentralWidget(globalSplitter);
 	this->setMinimumSize(900, 540);
 	timer->start(1000);
-
+	
+	//槽函数集群
 	connect(timer, SIGNAL(timeout()), this, SLOT(showCurDateTime()));
+	connect(visionUi.actionUndo, SIGNAL(triggered()), this, SLOT(Undo()));
+	connect(visionUi.actionRedo, SIGNAL(triggered()), this, SLOT(Redo()));
+	connect(visionUi.actionCut, SIGNAL(triggered()), this, SLOT(Cut()));
+	connect(visionUi.actionCopy, SIGNAL(triggered()), this, SLOT(Copy()));
+	connect(visionUi.actionPaste, SIGNAL(triggered()), this, SLOT(Paste()));
+	connect(visionUi.actionSelectAll, SIGNAL(triggered()), this, SLOT(SelectAll()));
+	connect(visionUi.actionDelete, SIGNAL(triggered()), this, SLOT(Delete()));
+	connect(visionUi.actionGetCode, SIGNAL(triggered()), this, SLOT(getCode()));
+	connect(visionUi.actionNew, SIGNAL(triggered()), this, SLOT(New()));
+	connect(visionUi.actionOpen, SIGNAL(triggered()), this, SLOT(Open()));
+	connect(visionUi.actionSave, SIGNAL(triggered()), this, SLOT(Save()));
+	connect(visionUi.actionSaveAll, SIGNAL(triggered()), this, SLOT(SaveAll()));
+	connect(visionUi.actionSaveAs, SIGNAL(triggered()), this, SLOT(SaveAs()));
+	connect(visionUi.actionClose, SIGNAL(triggered()), this, SLOT(Close()));
+	connect(visionUi.actionQuit, SIGNAL(triggered()), this, SLOT(Quit()));
+	connect(visionUi.actionAbout, SIGNAL(triggered()), this, SLOT(About()));
+
 }
 
 Vision::~Vision() {
@@ -36,12 +54,117 @@ Vision::~Vision() {
 	delete timer;
 	delete curDateTimeLabel;
 	delete a;
-	delete b;
-	delete c;
-	delete VSplitter;
+	delete plotTab;
+	delete codeTab;
+	delete globalSplitter;
 }
 
 void Vision::showCurDateTime() {
 	QString curDateTime = QDate::currentDate().toString() + " / " + QTime::currentTime().toString();
 	curDateTimeLabel->setText(curDateTime);
 }
+/*退出程序*/
+int Vision::Quit() {
+	int choose = -1;
+	if (true) {
+		choose = QMessageBox::question(this
+			, QString::fromLocal8Bit("退出")
+			, QString::fromLocal8Bit("是否保存所有文件？")
+			, QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel);
+		if (QMessageBox::Yes == choose)  SaveAll();
+		if (choose != QMessageBox::Cancel) qApp->quit();
+	}
+	else {
+		qApp->quit();
+	}
+	return choose;
+}
+/*窗口按钮退出*/
+void Vision::closeEvent(QCloseEvent* event) {
+	if (QMessageBox::Cancel == Quit())event->ignore();
+}
+/*关于*/
+void Vision::About() {
+	QMessageBox::information(this, QString::fromLocal8Bit("说明"), 
+		QString::fromLocal8Bit("Vision为您服务！\n使用：\n开发团队：\n"));
+}
+/*撤回*/
+void Vision::Undo() {
+
+}
+/*重做*/
+void Vision::Redo() {
+
+}
+/*剪切*/
+void Vision::Cut() {
+
+}
+/*复制*/
+void Vision::Copy() {
+
+}
+/*粘贴*/
+void Vision::Paste() {
+
+}
+/*全选*/
+void Vision::SelectAll() {
+
+}
+/*删除*/
+void Vision::Delete() {
+
+}
+/*复制代码*/
+void Vision::getCode() {
+
+}
+/*新建文件*/
+void Vision::New() {
+
+}
+/*打开文件*/
+void Vision::Open() {
+
+}
+/*保存文件*/
+void Vision::Save() {
+
+}
+/*保存全部*/
+void Vision::SaveAll() {
+
+}
+/*导出，另存为*/
+void Vision::SaveAs() {
+
+}
+/*关闭文件*/
+void Vision::Close() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Vision/Vision.h
+++ Vision/Vision.h
@@ -16,14 +16,37 @@ public:
 	~Vision();
 	QLabel* curDateTimeLabel;
 	QTimer* timer;
+
 public slots:
+	void About();
 	void showCurDateTime();
+
+protected:
+	void closeEvent(QCloseEvent* event)override;
+
 private:
 	Ui::VisionClass visionUi;
-	QSplitter* VSplitter;//左右竖直分割器
-	QTextEdit* a, * b, * c;//test
+	QSplitter* globalSplitter;//左右竖直分割器
+	QTextEdit* a;//test
+	QTabWidget* plotTab, * codeTab;
 
-private slots://槽函数声明
+private slots:
+	//大写是为避免和qt自带函数重名
+	void Undo();
+	void Redo();
+	void Cut();
+	void Copy();
+	void Paste();
+	void SelectAll();
+	void Delete();
+	void New();
+	void Open();
+	void Save();
+	void SaveAll();
+	void SaveAs();
+	void Close();
+	int Quit();
+	void getCode();
 
 
 };
